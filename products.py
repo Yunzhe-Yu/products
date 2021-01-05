8160c6c381990cdaafa31ea28030ae8d9234d640
Yunzhe Yu <yunzheyu0529@gmail.com>
2021-01-04 23:38:33 -0800

    add new function

---
+++ products.py
@@ -0,0 +1,21 @@
+products = [] # create a new list
+
+# 2-D list
+# insert a list into the first list
+
+
+while True:
+	name = input('Please enter the name of the item: ')
+	if name == 'q': # quit
+		break
+	price = input('Please enter the price of this item: ')
+	product = [] # new list
+	product.append(name)
+	product.append(price)
+	# product = [name, price]
+	products.append(product)
+	# products.append([name, price])
+
+print(products)
+
+print(products[0][0])
