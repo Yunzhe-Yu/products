124db3e1bbe7ee3a5c834f0d9646dffff1134ff2
Yunzhe Yu <yunzheyu0529@gmail.com>
2021-01-05 15:04:48 -0800

    csv file

--- products.py
+++ products.py
@@ -9,6 +9,7 @@ while True:
 	if name == 'q': # quit
 		break
 	price = input('Please enter the price of this item: ')
+	price = int(price)
 	product = [] # new list
 	product.append(name)
 	product.append(price)
@@ -20,3 +21,24 @@ print(products)
 
 for p in products:
 	print('The price of', p[0], 'is', p[1], 'dollar!')
+
+
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+# with open('products_price.txt', 'w') as f:
+with open('products_price.csv', 'w', encoding = 'utf-8') as f:
+	f.write('Items, Price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # f.write: write function
+
+# Can not plus integer to string use str() and int()
+# use encoding = utf - 8 can write chinese *****
+
+
+
+
+
+
+
+
